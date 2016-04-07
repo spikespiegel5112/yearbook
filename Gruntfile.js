00cc07b5b3787debb1111f8f0e0665b51df6188d
spikespiegel5112 <583288277@qq.com>
2016-04-08 07:35:24 +0800

    modified gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -384,8 +384,9 @@ module.exports = function(grunt) {
     grunt.registerTask('compile-css', ['cssmin', 'clean:sourceMap']);
     grunt.registerTask('compile-js', ['concat', 'uglify']);
     grunt.registerTask('compile', ['clean:all', 'useminPrepare', 'compile-js', 'compile-css', 'copy', 'usemin'/*,'px2rem'*/]);
-    grunt.registerTask('build', ['compile']);
-    grunt.registerTask('default', ['build']);
+    grunt.registerTask('build2',['clean:all'/*, 'useminPrepare'*/, 'concat', 'cssmin', 'copy', 'usemin']);
+    // grunt.registerTask('default', ['build']);
+    grunt.registerTask('default', ['build2']);
     grunt.registerTask('publish', ['build', 'sftp-deploy']);
     grunt.registerTask('server', ['build',  'connect:srcServer', 'connect:distServer', 'watch']);
 

724b9e101d1911b6ee67a602275a714605cfdb3c
spikespiegel5112 <583288277@qq.com>
2016-04-18 06:35:26 +0800

    add data replacement

--- src/js/base.js
+++ src/js/base.js
@@ -459,7 +459,7 @@
 							counter++;
 							break;
 					}
-                    e.stopPropagation();
+					e.stopPropagation();
 					counterEl.val(counter);
 					setter(index, counter);
 					config.onchange();
@@ -860,7 +860,7 @@
 						if (value > config.density + initVal) {
 							value = config.density + initVal;
 							/*console.log(Number(config.density) + Number(initVal));*/
-						} else if (value < 0||value<initVal) {
+						} else if (value < 0 || value < initVal) {
 							value = initVal;
 						} else if (isNaN(value)) {
 							value = config.density + initVal;
@@ -1092,30 +1092,33 @@
 			}
 		},
 		editphoto: function(config) {
+			var initConfig = config;
 			var transformData = {
-					scale: 1,
-					rotate: 0,
-					left: 0,
-					top: 0
+				scale: 1,
+				rotate: 0,
+				left: 0,
+				top: 0
+			}
+
+			var config = $.extend({
+				image: '',
+				tools: {
+					photoFrame: '',
+					turnRightBtn: '',
+					turnLeftBtn: '',
+					zoomInBtn: '',
+					zoomOutBtn: '',
+					reScaleBtn: '',
+					rePositionBtn: '',
+					originalSizeBtn: '',
+					scrollBar: ''
 				},
-				config = $.extend({
-					image: '',
-					tools: {
-						photoFrame: '',
-						turnRightBtn: '',
-						turnLeftBtn: '',
-						zoomInBtn: '',
-						zoomOutBtn: '',
-						reScaleBtn: '',
-						rePositionBtn: '',
-						originalSizeBtn: '',
-						scrollBar: ''
-					},
-					destroy: false
-				}, config);
+				destroy: false
+			}, config);
 			if (config.destroy == true) {
 				destroy();
 			};
+
 			var windowWidth = $(window).width(),
 				windowHeight = $(window).height(),
 				image = $(config.image),
@@ -1132,20 +1135,14 @@
 				originalHeight = 0,
 				originalRatio = 0,
 				saveRatio = 0,
-				epscrollbarWidth,
-				zoomingSlider,
-				sliderPosX,
+
 				initMarginleft = 0,
 				marginleft = 0;
 
-			if (config.tools.scrollBar != '') {
-				epscrollbarWidth = $(config.tools.scrollBar).find('label').width(),
-					zoomingSlider = $(config.tools.scrollBar).find('span'),
-					sliderPosX = zoomingSlider.offset().left * transformData.scale;
-				//	sliderPosX=(windowWidth-(windowWidth*0.8))/2;
-			} else {
-				return;
-			}
+
+			var epscrollbarWidth = $(config.tools.scrollBar).find('label').width(),
+				zoomingSlider = $(config.tools.scrollBar).find('span'),
+				sliderPosX = zoomingSlider.offset().left * transformData.scale;
 
 			init();
 
@@ -1153,6 +1150,7 @@
 				image.css('transition', 'transform 0.5s');
 				image.css(transformData);
 				var photoframe = $(config.tools.photoFrame);
+
 				if (frameRatio >= 1) {
 					photoframe.css({
 						width: $(window).width() * 0.9,
@@ -1253,6 +1251,21 @@
 				image.css(transformData);
 			});
 
+			if (initConfig == 'transformData') {
+				alert('aaa')
+				switch (config) {
+					case 'transformData':
+						alert('aaa');
+						return {
+							'transform': 'rotate(' + transformData.rotate + 'deg) scale(' + transformData.scale + ')',
+							'-webkit-transform': 'rotate(' + transformData.rotate + 'deg) scale(' + transformData.scale + ')',
+							'position': 'absolute',
+							'left': transformData.left,
+							'top': transformData.top
+						}
+				}
+			};
+
 			function sliderEvent(e) {
 				var touch = e.originalEvent.touches[0];
 				var movingPosX = touch.pageX;
@@ -1307,7 +1320,6 @@
 								imageTop = Number(image.css('top').replace('px', ''))
 							break;
 						case 'touchmove':
-							console.log('image touchmove')
 							e.preventDefault;
 							endPosX = touch.pageX,
 								endPosY = touch.pageY,
@@ -1333,8 +1345,8 @@
 				if (transformData.rotate || transformData.scale) {
 					$.extend(transformData, {
 						'transform': 'rotate(' + transformData.rotate + 'deg) scale(' + transformData.scale + ')'
-					})
-					console.log(transformData);
+					});
+					// console.log(transformData);
 					return transformData;
 				};
 			}
@@ -1353,6 +1365,7 @@
 				touchmoveEvent = null;
 				$('.action_btn').off();
 			}
+
 		}
 	});
 	$('.manage_tab').toolsSlide('.bannerslider_container', '.manage_tab .bannerslider_inner', '.manage_tab .bs_arrowbtn_left', '.manage_tab .bs_arrowbtn_right', 40);

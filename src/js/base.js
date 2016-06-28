e024b407a21e5deba4300f2b834ccb3b96ce7996
spikespiegel5112 <spikespiegel5112@hotmail.com>
2016-06-28 22:45:16 +0800

    temp

--- src/js/base.js
+++ src/js/base.js
@@ -1189,24 +1189,34 @@
 		collideLoading: function(options) {
 			options = $.extend({
 				onScrollBottom: function() {
-					alert('已触发底部但没有任何操作')
+					alert('已触发底部但没有任何操作');
 				}
-			}, options)
+			}, options);
+
+			var doc = document,
+				docEle = doc.documentElement,
+				docBody = doc.body,
+				docHeightContext = doc.body ? doc.body : docEle,
+				
+				clientHeight = 0,
+				windowHeight = doc.all ? docEle.offsetHeight : window.innerHeight;
+
 			window.onscroll = function() {
-				var clientHeight = 0,
-					scrollTop = document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop,
-					docHeight = document.scrollHeight ? document.scrollHeight : document.documentElement.scrollHeight;
+				var scrollTop = docEle.scrollTop || window.pageYOffset || docBody.scrollTop,
+					docHeight = docHeightContext.scrollHeight;
 
-				if (document.documentElement.clientHeight && document.body.clientHeight) {
-					clientHeight = Math.min(document.documentElement.clientHeight, document.body.clientHeight);
+				if (docBody.clientHeight && docEle.clientHeigh) {
+					clientHeight = Math.min(docBody.clientHeight, docEle.clientHeight);
 				} else {
-					clientHeight = Math.max(document.documentElement.clientHeight, document.body.clientHeight);
+					clientHeight = Math.max(docBody.clientHeight, docEle.clientHeight);
 				}
 
-				if (clientHeight + scrollTop >= docHeight) {
-					options.onScrollBottom()
+				clientHeight = window.innerHeight;
+
+				console.log(windowHeight + ' ' + scrollTop + ' ' + docHeight)
+				if (windowHeight + scrollTop >= docHeight) {
+					options.onScrollBottom();
 				}
-				console.log(docHeight);
 			}
 		},
 		orientationSensor: function(callback) {
